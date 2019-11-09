4c53adf6f0217da669290c6fbc637fe59f42b7aa
andres-zartab <andrezb5@gmail.com>
2019-11-09 15:36:10 -0500

    upload data to BQ + lang flag enabled

--- src/book_dataset.py
+++ src/book_dataset.py
@@ -1,11 +1,26 @@
+import argparse
+import os
 import json
 import requests
+import pandas as pd
+from google.cloud import bigquery
 
+parser = argparse.ArgumentParser(description='Uploads book dataset to BQ')
+
+parser.add_argument(
+    '--lang',
+    dest='lang',
+    type=str,
+    default='es',
+    help='provide a language code for the description of the books')
+
+args = parser.parse_args()
+lang = args.lang
 
 tlac_categories = ['business', 'philosophy', 'science',
                    'poetry', 'anthropology', 'art',
                    'sociology', 'education', 'history',
-                   'literature', 'psychology', 'theatre']
+                   'literature', 'psychology', 'theater']
 
 
 class book_category_iterator():
@@ -17,7 +32,7 @@ class book_category_iterator():
 
     def search_books_by_category(self):
         response = requests.get('https://www.googleapis.com/books/v1/volumes?q=subject:' +
-                                self.category + '&langRestrict=es&startIndex=' + str(self.start_index) + '&maxResults=40')
+                                self.category + '&langRestrict=' + lang + '&startIndex=' + str(self.start_index) + '&maxResults=40')
         if response.status_code == 200:
             self.start_index += 40
             return json.loads(response.content.decode('utf-8'))
@@ -62,40 +77,37 @@ class book_category_iterator():
             self.parse_book_info(json_books)
 
 
-'''
-fiction = book_category_iterator('fiction')
-fiction.main_loop()
-'''
+def book_list_to_df(book_list, cat):
+    titles = []
+    descs = []
+    cats = [cat for num in range(len(book_list))]
+
+    for book in book_list:
+        titles.append(book.get('title'))
+        descs.append(book.get('description'))
+
+    book_dict = {'category': cats,
+                 'title': titles,
+                 'description': descs}
+    return pd.DataFrame.from_dict(book_dict)
 
 
-class fill_per_category():
+to_upload_df = pd.DataFrame(columns=['category', 'title', 'description'])
 
-    def __init__(self, category):
-        self.category = category
-        self.info = []
+for cate in tlac_categories:
+    bki = book_category_iterator(cate)
+    bki.main_loop()
+    bl = bki.book_list
+    to_upload_df = to_upload_df.append(book_list_to_df(bl, cate))
 
-    def run(self):
-        process = book_category_iterator(self.category)
-        process.main_loop()
-        self.info.append(process.book_list)
-
-        return self.info
+to_upload_df.reset_index(drop=True, inplace=True)
 
 
-results = []
-for i in tlac_categories:
-    rs = fill_per_category(i)
-    results.append(rs.run())
+def append_table(table_name, df):
+    client = bigquery.Client()
+    dataset_ref = client.dataset('book_backend')
+    table_ref = dataset_ref.table(table_name)
+    client.load_table_from_dataframe(df, table_ref).result()
 
-# print(results)
 
-# crear pandas dataframe
-
-# import pandas as pd
-# from google.cloud import bigquery
-
-# def append_table(table_name, df):
-#     client = bigquery.Client()
-#     dataset_ref = client.dataset('pqf_lab')
-#     table_ref = dataset_ref.table(table_name)
-#     client.load_table_from_dataframe(df, table_ref).result()
+append_table('train_categories_' + lang, to_upload_df)
